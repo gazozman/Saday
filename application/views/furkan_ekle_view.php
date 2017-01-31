86d560a7b930bd6d113a8d0ddda56002300ba44b
Muhammed Salih Saday <yukleoyna@gmail.com>
2017-01-31 21:57:49 +0200

    Deneme Proje sonlandırıldı.

--- application/views/furkan_ekle_view.php
+++
@@ -1,5 +0,0 @@
-<?php echo form_open('furkan/veriEkle'); ?>
-İsim: <input type="text" name="isim_input">
-Soy İsim: <input type="text" name="soyisim_input">
-<button type="submit" name="buton_ismi" value="Sent">Kaydet</button>
-</form>
--- application/views/furkan_view.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<?php foreach($veri as $row){ ?>
-ID: <?php echo $row->id;?> </br>
-İsim: <?php echo $row->isim;?> </br>
-Soy İsim: <?php echo $row->soyisim;?>
-<a href="<?php echo base_url(); ?>Furkan/Detay/<?php echo $row->id;?>">Detaylar</a>
-<a href="<?php echo base_url(); ?>Furkan/veriSil/<?php echo $row->id;?>">Sil</a>
-</br></br>
-<?php } ?>
